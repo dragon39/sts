857bfd17555abde0aaead1f4bc032f0796a1bfde
sebastian.kondrat <sebastian.kondrat@asseco.pl>
2017-01-13 16:43:50 +0100

    Walidacja (str 71)

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -2,6 +2,8 @@ package masterSpringMvc.controller;
 
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,7 +11,10 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.*;
 
 @Controller
@@ -29,6 +34,17 @@ public class TweetController {
 		model.addAttribute("tweets", tweets);
 		model.addAttribute("search",search);
 		return "resultPage";
+	}
+	
+	@RequestMapping(value="/postSearch",method= RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+		String search = request.getParameter("search");
+		if(search.toLowerCase().contains("śmierć")){
+			redirectAttributes.addFlashAttribute("error", "Spróbuj wpisać Spring!");
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
 	}
 
 }
